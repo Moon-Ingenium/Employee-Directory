9aeccbe8ad8c4389a01b55f8d7ece73560f1c045
Michelle Moon <robbins.michelle@ymail.com>
2020-11-17 15:10:44 -0600

    boiler point

--- employee-directory/src/App.js
+++ employee-directory/src/App.js
@@ -1,12 +1,40 @@
+import React, { Component } from "react";
 import logo from './logo.svg';
 import './App.css';
+import Table from "./components/Table";
+import Row from "./components/Row";
+import employees from "./employees.json";
 
-function App() {
-  return (
-    <div className="App">
-      
-    </div>
-  );
+class App extends Component {
+  state = {
+    employees
+  };
+
+  // removeEmployee = id => {
+  //   // Filter this.state.friends for friends with an id not equal to the id being removed
+  //   const employees = this.state.employees.filter(employee => employee.id !== id);
+  // Set this.state.friends equal to the new friends array
+  //   this.setState({ employees });
+  // };
+
+  // Map over this.state.friends and render a FriendCard component for each friend object
+  render() {
+    return (
+      <Table>
+
+        {this.state.employees.map(employee => (
+          <Row
+          // // removeFriend={this.removeFriend}
+          id={employee.id}
+          key={employee.id}
+          name={employee.name}
+            occupation={employee.occupation}
+          location={employee.location}
+          />
+        ))}
+      </Table>
+    );
+  }
 }
 
 export default App;
---
+++ employee-directory/src/components/Table.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+function Table(props) {
+    return (
+        <table class="table table-dark">
+            <thead>
+                <tr>
+                    <th scope="col">Name</th>
+                    <th scope="col">Occupation</th>
+                    <th scope="col">Location</th>
+                </tr>
+            </thead>
+
+        </table>
+    );
+}
+export default Table;
